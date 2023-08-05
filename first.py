af79b21c2f834cb339ea1b40ac6e42535458a338
Phacekid <phacevictor@gmail.com>
2023-08-05 01:11:49 +0100

    first.py edited

--- first.py
+++ first.py
@@ -1,37 +1,44 @@
 import os
 import openai
 import sys
-sys.path.append('../..')
-
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv()) # read local .env file
-
-openai.api_key  = os.environ['OPENAI_API_KEY']
-
 from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+import uuid
+
+sys.path.append('../..')
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key  = os.environ['OPENAI_API_KEY']
 
 loader = PyPDFDirectoryLoader('./datasets')
 documents = loader.load()
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(
-    # separator="\n",
     chunk_size=1500,
     chunk_overlap=150,
-    # length_function=len
 )
 
 docs = text_splitter.split_documents(documents)
-
 print(f"{len(docs)} is length of docs")
 
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
+# Create a list of unique ids for each document based on the content
+ids = [str(uuid.uuid5(uuid.NAMESPACE_DNS, doc.page_content)) for doc in docs]
+unique_ids = list(set(ids))
+
+# Ensure that only docs that correspond to unique ids are kept and that only one of the duplicate ids is kept
+seen_ids = set()
+unique_docs = [
+    doc for doc, id in zip(docs, ids) 
+    if id not in seen_ids and (seen_ids.add(id) or True)
+    ]
+
 embedding = OpenAIEmbeddings()
 persist_directory = 'docs/chroma/'
 vectordb = Chroma.from_documents(
     documents=docs,
     embedding=embedding,
+    ids=unique_ids,
     persist_directory=persist_directory
 )
 vectordb.persist()
